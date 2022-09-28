69195862f8be6a7015c911f0c15c4d87b293ba9b
juanchristian2711 <Juanchristian2002@outlook.com>
2022-09-28 10:33:21 +0100

    Adding in a basic calendar with simple styling

--- minimalcalendar/src/App.js
+++ minimalcalendar/src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
+import {useState} from 'react';
+import 'react-calendar/dist/Calendar.css'
+import Calendar from 'react-calendar';
 import './App.css';
 
 function App() {
+  const [date, setDate] = useState(new Date())
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="header">
+        <h1>Calendar</h1>
       </header>
+      <div className='calendar-container'>
+        <Calendar onChange={setDate} value={date}/>
+      </div>
+      <div className='text-center'>
+        Selected date = {date.toDateString()}
+      </div>
     </div>
   );
 }
